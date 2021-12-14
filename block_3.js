f50fe276db27a8410e7c073063de61399888e536
yura_sts <yury.sts@gmail.com>
2021-12-14 17:41:52 +0100

    Start Master

---
+++ block_3.js
@@ -0,0 +1,8 @@
+/*Практические задания
+
+Практические задания для каждого блока должны выполнятся в отдельных ветках и предоставляться на проверку в виде pull request. Задачи необходимо делать .js файлах. Для вывода информации использовать console.log()
+
+Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+Выполнить явное преобразование(number, string, boolean)*/
