c47a48cba2e03f19408ed270b57304df0953be08
yura_sts <yury.sts@gmail.com>
2021-12-14 17:50:20 +0100

    Exercise block_3

--- block_3.js
+++ block_3.js
@@ -1,8 +1,46 @@
-/*Практические задания
+/*Дано:*/
+var str = "Условие или результат? ";            /* string */
+var bool = 4 < 1;                               /* boolean */
+var num = 3;                                    /* number */
 
-Практические задания для каждого блока должны выполнятся в отдельных ветках и предоставляться на проверку в виде pull request. Задачи необходимо делать .js файлах. Для вывода информации использовать console.log()
+console.log(str);
+console.log(bool);
+console.log(num);
 
-Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
-Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
-Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
-Выполнить явное преобразование(number, string, boolean)*/
+/*1. Выполнить сложение различных типов (string + boolean, string + number, number + boolean)*/
+var result1 = str + bool;                       /* string + boolean */
+console.log(typeof str,' + ', typeof bool,'->', typeof result1,':', result1);
+var result2 = str + num;                        /* string + number */
+console.log(typeof str,' + ', typeof num ,' ->', typeof result2,':', result2);
+var result3 = num + bool;                       /* number + boolean */
+console.log(typeof num,' + ', typeof bool,'->', typeof result3,':', result3);
+
+/*2.Выполнить умножение различных типов (string * boolean, string * number, number * boolean)*/
+var result4 = str * bool;                       /* string * boolean */
+console.log(typeof str,' * ', typeof bool,'->', typeof result4,':', result4);
+var result5 = str * num;                        /* string * number */
+console.log(typeof str,' * ', typeof num ,' ->', typeof result5,':', result5);
+var result6 = num * bool;                       /* number * boolean */
+console.log(typeof num,' * ', typeof bool,'->', typeof result6,':', result6);
+
+/*3. Выполнить деление различных типов (string / boolean, string / number, number / boolean)*/
+var result7 = str / bool;                       /* string / boolean */
+console.log(typeof str,' / ', typeof bool,'->', typeof result7,':', result7);
+var result8 = str / num;                        /* string / number */
+console.log(typeof str,' / ', typeof num ,' ->', typeof result8,':', result8);
+var result9 = num / bool;                       /* number / boolean */
+console.log(typeof num,' / ', typeof bool,'->', typeof result9,':', result9);
+
+/*Выполнить явное преобразование (number, string, boolean)*/
+var result10 = String(num);                     /* number -> string */
+console.log(typeof num ,' -> ', typeof result10,' :', result10);
+var result11 = String(bool);                    /* boolean -> string */
+console.log(typeof bool,'-> ', typeof result11,' :', result11);
+var result12 = Number(str);                     /* string -> number */
+console.log(typeof str ,' -> ', typeof result12,' :', result12);
+var result13 = Number(bool);                    /* boolean -> number */
+console.log(typeof bool,'-> ', typeof result13,' :', result13);
+var result14 = Boolean(num);                    /* number -> boolean */
+console.log(typeof num ,' -> ', typeof result14,':', result14);
+var result15 = Boolean(str);                    /* number -> boolean */
+console.log(typeof str ,' -> ', typeof result15,':', result15);
